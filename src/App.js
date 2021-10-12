920aa1a4169668826368983550c93dbde61f7abb
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-10-12 16:16:52 +0600

    facebook authentication added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { getAuth, GoogleAuthProvider, signInWithPopup, GithubAuthProvider, FacebookAuthProvider, signOut } from 'firebase/auth';
+import { useState } from 'react';
 import './App.css';
+import initializeAuthentication from './firebase/firebase.initialize';
+
+initializeAuthentication();
+const GoogleProvider = new GoogleAuthProvider();
+const GitHubProvider = new GithubAuthProvider();
+const FacebookProvider = new FacebookAuthProvider();
+
+
 
 function App() {
+  const [user, setUser] = useState({})
+  const auth = getAuth();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, GoogleProvider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const logedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(logedInUser)
+      }).catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, GitHubProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        console.log(result);
+        const logedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(logedInUser)
+      }).catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
+
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, FacebookProvider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        console.log(result.user);
+        const logedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(logedInUser)
+      })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      }).catch((error) => {
+        // An error happened.
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {!user.name ? <div>
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        <button onClick={handleGithubSignIn}>Github Sign In</button>
+        <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
+      </div> :
+        <button onClick={handleSignOut}>Sign Out</button>}
+      <br />
+      {user.name && <div>
+        <h1>Welcome {user.name}</h1>
+        <h3>I know your email: {user.email}</h3>
+        <img src={user.photo} alt="" />
+      </div>}
+
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBPJQqwMtDPc3hsFy-ATfxrtqHlAO90VRk",
+    authDomain: "simple-firebase-authenti-f10a3.firebaseapp.com",
+    projectId: "simple-firebase-authenti-f10a3",
+    storageBucket: "simple-firebase-authenti-f10a3.appspot.com",
+    messagingSenderId: "955491174124",
+    appId: "1:955491174124:web:63b9fd6f24b318a570c7c9"
+};
+
+export default firebaseConfig;
